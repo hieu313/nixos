e891fd6e343c9007264ad8fdb80b40ae818c938d
epic9491 <epic9491@tutamail.com>
2025-11-26 17:55:09 -0600

    switched syncthing to per device configuration

--- devices/laptop/prometheus/default.nix
+++ devices/laptop/prometheus/default.nix
@@ -15,4 +15,49 @@
   networking.hostName = "prometheus";
 
   # Prometheus Tweaks
+
+  services.syncthing = {
+    enable = true;
+    user = "gumbo";
+    group = "users";
+    dataDir = "/home/gumbo/sync";
+    configDir = "/home/gumbo/.config/syncthing";
+    overrideDevices = false;
+    overrideFolders = false;
+    openDefaultPorts = true;
+    settings = {
+      devices = {
+        "manga" = {
+          id = "I3J5UCJ-NZIOJCX-FIV6PUT-QSTITFA-4TI6PB7-MVR67TI-SW56QXD-6ARBAAE";
+        };
+      };
+      folders = {
+        "retro-bios" = {
+          id = "p4epq-mmgmv";
+          path = "/home/gumbo/sync/retro/BIOS";
+          devices = [
+            "manga"
+          ];
+          type = "receiveonly";
+        };
+        "retro-roms" = {
+          id = "74edp-unucu";
+          path = "/home/gumbo/sync/retro/ROMs";
+          devices = [
+            "manga"
+          ];
+          type = "receiveonly";
+        };
+        "retro-saves" = {
+          id = "ymtp3-m4ngw";
+          path = "/home/gumbo/sync/retro/Saves";
+          devices = [
+            "manga"
+          ];
+          type = "sendreceive";
+        };
+      };
+    };
+  };
+
 }
--- profiles/common.nix
+++ profiles/common.nix
@@ -114,56 +114,6 @@
     libinput.enable = true;
   };
 
-  services.syncthing = {
-    enable = true;
-    user = "gumbo";
-    group = "users";
-    dataDir = "/home/gumbo/sync";
-    configDir = "/home/gumbo/.config/syncthing";
-    overrideDevices = false;
-    overrideFolders = false;
-    openDefaultPorts = true;
-    settings = {
-      devices = {
-        "manga" = {
-          id = "I3J5UCJ-NZIOJCX-FIV6PUT-QSTITFA-4TI6PB7-MVR67TI-SW56QXD-6ARBAAE";
-        };
-        "erebos" = {
-          id = "DBCLUXM-MWHS7OZ-AJIDHAY-GFGTMDW-5RBCU4P-M4RE3IF-YMDWWOB-DNCSQQT";
-        };
-      };
-      folders = {
-        "retro-bios" = {
-          id = "p4epq-mmgmv";
-          path = "/home/gumbo/sync/retro/BIOS";
-          devices = [
-            "manga"
-            "erebos"
-          ];
-          type = "receiveonly";
-        };
-        "retro-roms" = {
-          id = "74edp-unucu";
-          path = "/home/gumbo/sync/retro/ROMs";
-          devices = [
-            "manga"
-            "erebos"
-          ];
-          type = "receiveonly";
-        };
-        "retro-saves" = {
-          id = "ymtp3-m4ngw";
-          path = "/home/gumbo/sync/retro/Saves";
-          devices = [
-            "manga"
-            "erebos"
-          ];
-          type = "sendreceive";
-        };
-      };
-    };
-  };
-
   # virtualization
   programs.virt-manager.enable = true;
   virtualisation = {
