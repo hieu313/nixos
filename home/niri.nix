34ce135548ee0ae8d98c3d575e8ef2d9afc322fd
sensei <sensei@gumbolabs.xyz>
2025-12-01 01:12:53 -0600

    updated system

--- home/niri.nix
+++ home/niri.nix
@@ -31,14 +31,14 @@
   home.pointerCursor = {
     name = "BreezeX-RosePine-Linux";
     package = pkgs.rose-pine-cursor;
-    size = 28;
+    size = 24;
     gtk.enable = true;
     x11.enable = true;
   };
 
   home.sessionVariables = {
     XCURSOR_THEME = "BreezeX-RosePine-Linux";
-    XCURSOR_SIZE = "28";
+    XCURSOR_SIZE = "24";
   };
 
 }
