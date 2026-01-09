f676615dbda7d5d5156ec1f4552f08fe52be5763
sensei <sensei@gumbolabs.xyz>
2026-01-08 18:19:14 -0600

    udpate kde

--- profiles/kde/system.nix
+++ profiles/kde/system.nix
@@ -12,6 +12,7 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  services.displayManager.defaultSession = "plasma" ;
 
   services.displayManager.sddm.settings.General.DisplayServer = "wayland";
 
