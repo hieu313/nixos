6a78d77c73babc143f232656a024c69a90b364d8
sensei <sensei@gumbolabs.xyz>
2026-01-09 00:37:42 -0600

    fix kde

--- profiles/kde/system.nix
+++ profiles/kde/system.nix
@@ -10,11 +10,8 @@
   
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "plasma" ;
-
-  services.displayManager.sddm.settings.General.DisplayServer = "wayland";
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
   konsole
