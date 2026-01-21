ddfd105fd882004464ebe802810b43d204d744b5
sensei <sensei@gumbolabs.xyz>
2026-01-21 12:51:16 -0600

    update yazi config for niri

--- profiles/niri/system.nix
+++ profiles/niri/system.nix
@@ -16,6 +16,22 @@
       wl-clipboard = pkgs.yaziPlugins.wl-clipboard;
       chmod = pkgs.yaziPlugins.chmod;
       git = pkgs.yaziPlugins.git;
+      settings = {
+        yazi = {
+          ratio = [
+            1
+            4
+            3
+          ];
+          sort_by = "natural";
+          sort_sensitive = true;
+          sort_reverse = false;
+          sort_dir_first = true;
+          linemode = "none";
+          show_hidden = true;
+          show_symlink = true;
+        };
+      };
     };
   };
 
