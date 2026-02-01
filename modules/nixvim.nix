55114ddffd90e093476dbcb0f6be7b205df94911
sensei <sensei@gumbolabs.xyz>
2026-02-01 17:34:29 -0600

    cleared some errors, still works fine. need to get off snacks

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -13,7 +13,8 @@ in
     inputs.nixvim.nixosModules.nixvim
   ];
 
-  options.workstation.nixvim.enable = lib.mkEnableOption "Nvim configuration";
+  options.workstation.nixvim.enable =
+    lib.mkEnableOption "Nvim configuration";
 
   config = lib.mkIf cfg.enable {
     programs.nixvim = {
@@ -57,12 +58,9 @@ in
 
       plugins = {
         bufferline.enable = true;
-
         lualine.enable = true;
-
         mini.ai.enable = true;
         mini.pairs.enable = true;
-
         noice.enable = true;
         web-devicons.enable = true;
 
@@ -131,111 +129,107 @@ in
       ];
 
       extraConfigLua = ''
-                -- flash.nvim
-                require("flash").setup({})
+        -- flash.nvim
+        require("flash").setup({})
 
-                -- ts-comments
-                require("ts-comments").setup({})
+        -- ts-comments
+        require("ts-comments").setup({})
 
-                -- Snacks configuration without lazy.nvim integration
-                local snacks = require("snacks")
-                snacks.setup({
-                  lazy = {},
-                  dashboard = {
-                    enabled = true,
-                    width = 60,
-                    pane_gap = 4,
-                    autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                    preset = {
-                      pick = nil,
-                      keys = {
-                        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-                        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-                        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                        {
-                          icon = " ",
-                          key = "c",
-                          desc = "Config",
-                          action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-                        },
-                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-                        { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-                      },
-                      header = [[
-        ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-        ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-        ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-        ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-        ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-        ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-                    },
-                    formats = {
-                      icon = function(item)
-                        if (item.file and item.icon == "file") or item.icon == "directory" then
-                          return Snacks.dashboard.icon(item.file, item.icon)
-                        end
-                        return { item.icon, width = 2, hl = "icon" }
-                      end,
-                      footer = { "%s", align = "center" },
-                      header = { "%s", align = "center" },
-                      file = function(item, ctx)
-                        local fname = vim.fn.fnamemodify(item.file, ":~")
-                        fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
-                        if #fname > ctx.width then
-                          local dir = vim.fn.fnamemodify(fname, ":h")
-                          local file = vim.fn.fnamemodify(fname, ":t")
-                          if dir and file then
-                            file = file:sub(-(ctx.width - #dir - 2))
-                            fname = dir .. "/…" .. file
-                          end
-                        end
-                        local dir, file = fname:match("^(.*)/(.+)$")
-                        return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } }
-                          or { { fname, hl = "file" } }
-                      end,
-                    },
-                    sections = {
-                      { section = "header" },
-                      { section = "keys", gap = 1, padding = 1 },
-                      { section = "startup" },
-                    },
-                  },
-                  picker = {
-                    enabled = true,
-                    sources = {
-                      files = {
-                        hidden = true,
-                        ignored = false,
-                        exclude = { "**/.local/**", "**/.cache/**" },
-                      },
-                      grep = {
-                        hidden = true,
-                        ignored = false,
-                        exclude = { "**/.local/**", "**/.cache/**" },
-                      },
-                    },
-                  },
-                })
+        -- Snacks configuration without lazy.nvim integration
+        local snacks = require("snacks")
+        snacks.setup({
+          lazy = {
+            enabled = false,
+          },
+          dashboard = {
+            enabled = true,
+            width = 60,
+            pane_gap = 4,
+            autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            preset = {
+              pick = nil,
+              keys = {
+                { icon = " ", key = "f", desc = "Find File",  action = ":lua Snacks.dashboard.pick('files')" },
+                { icon = " ", key = "n", desc = "New File",   action = ":ene | startinsert" },
+                { icon = " ", key = "g", desc = "Find Text",  action = ":lua Snacks.dashboard.pick('live_grep')" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                -- removed the Config entry that caused the BufWipeout/autocmd error
+                { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+              },
+              header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+            },
+            formats = {
+              icon = function(item)
+                if (item.file and item.icon == "file") or item.icon == "directory" then
+                  return Snacks.dashboard.icon(item.file, item.icon)
+                end
+                return { item.icon, width = 2, hl = "icon" }
+              end,
+              footer = { "%s", align = "center" },
+              header = { "%s", align = "center" },
+              file = function(item, ctx)
+                local fname = vim.fn.fnamemodify(item.file, ":~")
+                fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+                if #fname > ctx.width then
+                  local dir = vim.fn.fnamemodify(fname, ":h")
+                  local file = vim.fn.fnamemodify(fname, ":t")
+                  if dir and file then
+                    file = file:sub(-(ctx.width - #dir - 2))
+                    fname = dir .. "/…" .. file
+                  end
+                end
+                local dir, file = fname:match("^(.*)/(.+)$")
+                return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } }
+                  or { { fname, hl = "file" } }
+              end,
+            },
+            sections = {
+              { section = "header" },
+              { section = "keys", gap = 1, padding = 1 },
+            },
+          },
+          picker = {
+            enabled = true,
+            sources = {
+              files = {
+                hidden = true,
+                ignored = false,
+                exclude = { "**/.local/**", "**/.cache/**" },
+              },
+              grep = {
+                hidden = true,
+                ignored = false,
+                exclude = { "**/.local/**", "**/.cache/**" },
+              },
+            },
+          },
+        })
 
-                -- command and keymap for dashboard
-                vim.api.nvim_create_user_command("SnacksDashboard", function()
-                  snacks.dashboard.open()
-                end, {})
+        -- command and keymap for dashboard
+        vim.api.nvim_create_user_command("SnacksDashboard", function()
+          snacks.dashboard.open()
+        end, {})
 
-                vim.keymap.set("n", "<leader>sd", function()
-                  snacks.dashboard.open()
-                end, { desc = "Snacks Dashboard" })
+        vim.keymap.set("n", "<leader>sd", function()
+          snacks.dashboard.open()
+        end, { desc = "Snacks Dashboard" })
 
-                -- auto-open dashboard on start with no file
-                vim.api.nvim_create_autocmd("VimEnter", {
-                  callback = function()
-                    if vim.fn.argc() == 0 then
-                      snacks.dashboard.open()
-                    end
-                  end,
-                })
+        -- auto-open dashboard on start with no file
+        vim.api.nvim_create_autocmd("VimEnter", {
+          callback = function()
+            if vim.fn.argc() == 0 then
+              snacks.dashboard.open()
+            end
+          end,
+        })
       '';
     };
   };
