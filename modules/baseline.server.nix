80d21ed48bcd0d5d1e49a9df6dc1e63cbe65ce6a
sensei <sensei@gumbolabs.xyz>
2026-02-09 23:30:51 -0600

    no editor on server lol

--- modules/baseline.server.nix
+++ modules/baseline.server.nix
@@ -59,6 +59,7 @@ in
     environment.systemPackages = with pkgs; [
       # tools/etc
       inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
+      vim
       wget
       git
       htop
