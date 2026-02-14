c64b1cd6c9290bacbed26d0332cb2d85f82b091c
sensei <sensei@gumbolabs.xyz>
2026-02-13 20:06:31 -0600

    update newt service to be more predictable

--- devices/server/v-gaia-main/newt.nix
+++ devices/server/v-gaia-main/newt.nix
@@ -5,18 +5,25 @@
   ...
 }:
 {
-
   services.newt = {
     enable = true;
     environmentFile = "/run/agenix/newt.env.age";
   };
-
+  
+  systemd.services.newt.serviceConfig.DynamicUser = lib.mkForce false;
+  
+  users.users.newt = {
+    isSystemUser = true;
+    group = "newt";
+  };
+  
+  users.groups.newt = {};
+  
   age.secrets."newt.env.age" = {
     file = ../../../secrets/newt.env.age;
     path = "/run/agenix/newt.env.age";
-    owner = "gumbo";
-    group = "users";
+    owner = "newt";
+    group = "newt";
     mode = "0400";
   };
-
 }
