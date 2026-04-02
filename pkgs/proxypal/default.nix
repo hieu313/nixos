e010b5c59d29a0a27f117a2bf9ac20e062b7945c
hieunm3103 <hieunm3103@gmail.com>
2026-04-02 13:31:46 +0700

    feat(proxypal): update package definition to include icon and improve structure

--- pkgs/proxypal/default.nix
+++ pkgs/proxypal/default.nix
@@ -1,18 +1,29 @@
-{ stdenv, fetchurl, lib, makeWrapper
-, libayatana-appindicator, gtk3, glib, webkitgtk_4_1
+{
+  stdenv,
+  fetchurl,
+  lib,
+  makeWrapper,
+  libayatana-appindicator,
+  gtk3,
+  glib,
+  webkitgtk_4_1,
 }:
+
 let
   pname = "proxypal";
   version = "1.0.0";
 in
 stdenv.mkDerivation {
   inherit pname version;
+
   src = fetchurl {
     url = "https://github.com/hieu313/nixos/releases/download/${version}/proxypal-binary-nixos";
     hash = "sha256-kujrgGLrtWn/8VcONHSn1uq0rtQs8QKvOZSeW48PlMg=";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [
+    makeWrapper
+  ];
 
   dontUnpack = true;
 
@@ -22,6 +33,9 @@ stdenv.mkDerivation {
     chmod +x $out/bin/proxypal
 
     mkdir -p $out/share/applications
+    mkdir -p $out/share/icons/hicolor/512x512/apps
+    cp ${./icon.png} $out/share/icons/hicolor/512x512/apps/proxypal.png
+
     cat > $out/share/applications/proxypal.desktop << EOF
 [Desktop Entry]
 Name=ProxyPal
@@ -50,6 +64,6 @@ EOF
     homepage = "https://github.com/hieu313/nixos";
     platforms = [ "x86_64-linux" ];
     license = licenses.unfree;
-    maintainers = [];
+    maintainers = [ ];
   };
 }
