30750dbb8648f64642d803eeaade9894e192827e
hieunm3103 <hieunm3103@gmail.com>
2026-04-12 12:40:05 +0700

    fix(update.sh): remove redundant sudo command for nixos-rebuild in update script

--- update.sh
+++ update.sh
@@ -6,4 +6,3 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 cp ~/.config/noctalia/*.json "$SCRIPT_DIR/config/noctalia/"
 cp ~/.config/yazi/package.toml "$SCRIPT_DIR/config/yazi/package.toml"
 sudo nixos-rebuild switch --flake .#aries
-sudo cp /boot/EFI/NixOS-boot/grubx64.efi /boot/EFI/ARCH/grubx64.efi
