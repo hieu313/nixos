d35dcf44549f512d02732e9aff429dd364f2f6af
hieunm3103 <hieunm3103@gmail.com>
2026-06-04 15:09:48 +0700

    feat(cursor): update cursor configuration to use dynamic values

--- home/desktop/niri.nix
+++ home/desktop/niri.nix
@@ -6,9 +6,28 @@
   ...
 }:
 let
-  niriConfig = if builtins.elem hostName [ "prometheus" "aries" ]
-    then ../../config/niri/config.laptop.kdl 
-    else ../../config/niri/config.desktop.kdl;
+  cursor = {
+    name = "Future-cursors";
+    package = pkgs.callPackage ../../pkgs/future-cursor.nix { };
+    size = 20;
+  };
+
+  niriConfigTemplate =
+    if
+      builtins.elem hostName [
+        "prometheus"
+        "aries"
+      ]
+    then
+      ../../config/niri/config.laptop.kdl
+    else
+      ../../config/niri/config.desktop.kdl;
+
+  niriConfig =
+    builtins.replaceStrings
+      [ "@cursorTheme@" "@cursorSize@" ]
+      [ cursor.name (builtins.toString cursor.size) ]
+      (builtins.readFile niriConfigTemplate);
 in
 {
   gtk = {
@@ -24,21 +43,21 @@ in
     };
 
     cursorTheme = {
-      name = "Bibata-Modern-Classic";
-      package = pkgs.bibata-cursors;
-      size = 20;
+      name = cursor.name;
+      package = cursor.package;
+      size = cursor.size;
     };
 
     gtk3.extraConfig = {
       "gtk-application-prefer-dark-theme" = true;
-      "gtk-cursor-theme-name" = "Bibata-Modern-Classic";
+      "gtk-cursor-theme-name" = cursor.name;
     };
 
     gtk4.extraConfig = {
       "gtk-application-prefer-dark-theme" = true;
       Settings = ''
-      gtk-cursor-theme-name=Bibata-Modern-Classic
-      gtk-cursor-theme-size=16
+        gtk-cursor-theme-name=${cursor.name}
+        gtk-cursor-theme-size=${builtins.toString cursor.size}
       '';
     };
   };
@@ -47,45 +66,45 @@ in
     enable = true;
     platformTheme.name = "qt6ct";
     style = {
-			name = "kvantum";
-			package = pkgs.kdePackages.qtstyleplugin-kvantum;
-		};
+      name = "kvantum";
+      package = pkgs.kdePackages.qtstyleplugin-kvantum;
+    };
   };
 
-	dconf.settings = {
-		"org/gnome/desktop/interface" = {
-			color-scheme = "prefer-dark";
-			gtk-theme = "Tokyonight-Dark";
-			icon-theme = "Papirus";
-			cursor-theme = "Bibata-Modern-Classic";
-			cursor-size = 20;
-		};
-	};
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "Tokyonight-Dark";
+      icon-theme = "Papirus";
+      cursor-theme = cursor.name;
+      cursor-size = cursor.size;
+    };
+  };
 
   xdg.configFile = {
     "gtk-3.0/settings.ini".force = true;
     "gtk-4.0/settings.ini".force = true;
     "gtk-4.0/gtk.css".force = true;
-    "niri/config.kdl".source = niriConfig;
+    "niri/config.kdl".text = niriConfig;
     "niri/noctalia.kdl".source = ../../config/niri/noctalia.kdl;
     "ghostty/config".source = ../../config/ghostty/tokyo-night.ghostty;
   };
 
   home.pointerCursor = {
-    name = "Bibata-Modern-Classic";
-    package = pkgs.bibata-cursors;
-    size = 20;
+    name = cursor.name;
+    package = cursor.package;
+    size = cursor.size;
     gtk.enable = true;
     x11.enable = true;
   };
 
   home.sessionVariables = {
-    XCURSOR_THEME = "Bibata-Modern-Classic";
-    XCURSOR_SIZE = "20";
+    XCURSOR_THEME = cursor.name;
+    XCURSOR_SIZE = builtins.toString cursor.size;
     NIXOS_OZONE_WL = "1";
     ICON_THEME = "Papirus";
     QS_ICON_THEME = "Papirus";
     QT_QPA_PLATFORM = "wayland";
-		GTK_THEME = "Tokyonight-Dark";
+    GTK_THEME = "Tokyonight-Dark";
   };
 }
--- modules/niri.nix
+++ modules/niri.nix
@@ -7,7 +7,7 @@
 }:
 let
   cfg = config.workstation.niri;
-	sddmCfg = config.workstation.sddm;
+  sddmCfg = config.workstation.sddm;
 in
 {
   options.workstation.niri.enable = lib.mkEnableOption "Niri-based workstation environment with Noctalia Shell";
@@ -19,26 +19,25 @@ in
       enable = true;
     };
 
-		environment.variables = {
+    environment.variables = {
       GTK_USE_PORTAL = "1";
-			NIXOS_OZONE_WL = "1";
-			XDG_SESSION_TYPE = "wayland";
-			XDG_CURRENT_DESKTOP = "niri";
-		};
+      NIXOS_OZONE_WL = "1";
+      XDG_SESSION_TYPE = "wayland";
+      XDG_CURRENT_DESKTOP = "niri";
+    };
 
     environment.systemPackages = with pkgs; [
       inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
       xwayland-satellite
       tokyonight-gtk-theme
       swayimg
-      rose-pine-cursor
       papirus-icon-theme
       nemo
       fuzzel
       gpu-screen-recorder
       wl-clipboard
       libsForQt5.qt5ct
-			kdePackages.qt6ct
+      kdePackages.qt6ct
       kdePackages.qtwayland
       libsForQt5.qtstyleplugin-kvantum
       kdePackages.qtstyleplugin-kvantum
